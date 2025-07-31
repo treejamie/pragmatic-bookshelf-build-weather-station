27a67c67345dd11b4d2384eae2de1d5870c94f28
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-07-31 21:47:07 +0100

    BME 680 working as expected (mostly) (#6)
    I need to fix altitude, but that's in #5

--- sensor_hub/lib/sensor_hub/sensor.ex
+++ sensor_hub/lib/sensor_hub/sensor.ex
@@ -11,15 +11,28 @@ defmodule SensorHub.Sensor do
   end
 
   def fields(SGP40), do: [:voc_index]
-  def fields(BME680), do: [:altitude_m, :pressure_pa, :temprature_c]
+
+  def fields(BME680),
+    do: [
+      :altitude_m,
+      :pressure_pa,
+      :temprature_c,
+      :humidity_rh,
+      :dew_point_c,
+      :gas_resistance_ohms
+    ]
+
   def fields(VEML7700), do: [:light_lumens]
 
   def read_fn(SGP40) do
     fn -> Process.whereis(:sgp40) |> SGP40.measure() end
   end
 
-  def read_fn(BME680), do: fn -> BMP280.measure(BME680) end
   def read_fn(VEML7700), do: fn -> Veml7700.get_measurement() end
+
+  def read_fn(BME680) do
+    fn -> Process.whereis(BME680) |> BMP280.measure() end
+  end
 
   def convert_fn(SGP40) do
     fn reading ->
@@ -37,7 +50,16 @@ defmodule SensorHub.Sensor do
     fn reading ->
       case reading do
         {:ok, measurement} ->
-          Map.take(measurement, [:altitude, :pressure_pa, :temperature_C])
+          Map.take(measurement, [
+            :altitude_m,
+            :pressure_pa,
+            :temprature_c,
+            :humidity_rh,
+            :dew_point_c,
+            :gas_resistance_ohms
+          ])
+
+        # TODO: fix altitude by correcting for sea level pressure. I am not -146m below sea level
 
         _ ->
           %{}
