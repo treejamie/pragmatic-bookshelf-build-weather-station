96df80220fd8135e02881ea43d34bf2379e4f1ab
Tree Jamie <me@treejamie.com>
2025-08-06 21:43:42 +0100

    Added controller and the route

--- tracker/lib/tracker_web/router.ex
+++ tracker/lib/tracker_web/router.ex
@@ -2,20 +2,21 @@ defmodule TrackerWeb.Router do
   use TrackerWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
-  scope "/api", TrackerWeb do
-    pipe_through :api
+  scope "/api/v1", TrackerWeb do
+    pipe_through(:api)
+
+    post("/sensor/data", DataController, :create)
   end
 
   # Enable Swoosh mailbox preview in development
   if Application.compile_env(:tracker, :dev_routes) do
-
     scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
+      pipe_through([:fetch_session, :protect_from_forgery])
 
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
