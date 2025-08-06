b5ceb9d545da60312dd47a45223045c08a7de71f
Tree Jamie <me@treejamie.com>
2025-08-06 21:01:54 +0100

    Schema in

---
+++ tracker/lib/tracker/sensors/data.ex
@@ -0,0 +1,41 @@
+defmodule Tracker.Sensors.Data do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @allowed_fields [
+    :timestamp,
+    :altitude_m,
+    :pressure_pa,
+    :temperature_c,
+    :humidity_rh,
+    :dew_point_c,
+    :gas_resistance_ohms,
+    :voc_index,
+    :lumens
+  ]
+
+  @derive {Jason.Encoder, only: @allowed_fields}
+  @primary_key false
+  schema :sensor_data do
+    field(:timestamp, :naive_datetime)
+    field(:altitude_m, :decimal)
+    field(:pressure_pa, :decimal)
+    field(:temperature_c, :decimal)
+    field(:humidity_rh, :decimal)
+    field(:dew_point_c, :decimal)
+    field(:gas_resistance_ohms, :decimal)
+    field(:voc_index, :decimal)
+    field(:lumens, :decimal)
+  end
+
+  def create_changeset(data = %Data{}, attrs) do
+    timestamp =
+      NaiveDateTime.utc_now()
+      |> NaiveDateTime.truncate(:second)
+
+    data
+    |> cast(attrs, @allowed_fields)
+    |> validate_required(@allowed_fields)
+    |> put_change(:timestamp, timestamp)
+  end
+end
--- tracker/mix.exs
+++ tracker/mix.exs
@@ -42,7 +42,8 @@ defmodule Tracker.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:decimal, "~> 2.0"}
     ]
   end
 
