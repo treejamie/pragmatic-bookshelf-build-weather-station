a486daf0562e99497f7ca189f6a7f5fe748788f4
Tree Jamie <me@treejamie.com>
2025-08-11 21:41:57 +0100

    Now ready to connect fireware

--- publisher/lib/publisher.ex
+++ publisher/lib/publisher.ex
@@ -1,18 +1,51 @@
 defmodule Publisher do
   @moduledoc """
-  Documentation for `Publisher`.
+  Apparently this will be laser focused
   """
+  use GenServer
+  require Logger
 
-  @doc """
-  Hello world.
+  def start_link(options \\ %{}) do
+    GenServer.start_link(__MODULE__, options, name: __MODULE__)
+  end
 
-  ## Examples
+  @impl true
+  def init(options) do
+    state = %{
+      interval: 10_000,
+      api_url: options[:api_url],
+      sensors: options[:sensors],
+      measurements: :no_measurements
+    }
+  end
 
-      iex> Publisher.hello()
-      :world
+  @impl true
+  def handle_info(:publish_data, state) do
+    {:noreply, state |> measure |> publish}
+  end
 
-  """
-  def hello do
-    :world
+  defp measure(state) do
+    data =
+      Enum.reduce(state.sensors, %{}, fn sensor, acc ->
+        sensor_data = sensor.read.() |> sensor.convert.()
+        Map.merge(acc, data)
+      end)
+
+    %{state | measurements: measurements}
+  end
+
+  defp publish(state) do
+    result =
+      Finch.build(
+        :post,
+        state.api_url,
+        [{"Content-Type", "application/json"}],
+        JSON.encode!(state.measurements)
+      )
+      |> Finch.request(TrackerClient)
+  end
+
+  defp schedule_publish(interval) do
+    Process.send_after(self(), :publish_data, interval)
   end
 end
